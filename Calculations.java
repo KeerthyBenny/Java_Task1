d6172dd9d07a4bc65a51f68fb98f3271e8af2bcb
avinash <mogiligidda.avinash@gmail.com>
2019-02-01 12:50:31 -0500

    collections concept calc

--- Calculations.java
+++ Calculations.java
@@ -1,83 +1,117 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Calculations {
 
-	private static String findString;
+
 
 	public static void main(String[] args) 
+
+	{
+
+		System.out.println("Welcome to Satapata Calculator");
+		List<String> hist = new ArrayList<String>();
+		loop(hist);
+
+	}
+	static boolean isInteger(String n)
+	{
+		try
+		{
+
+			Integer.parseInt(n);
+		}
+
+		catch(Exception ex)
+		{
+			return false;
+		}
+		return true;
+	}
+
+	private static void loop(List<String> hist)
+
 	{
 
 		Scanner s= new Scanner(System.in);
 		System.out.println("enter the string");
 		String str = s.nextLine();
-		String array[]= str.split(" ");
-		int[] numberCount = new int [2];
-		int count=0;
-
-		for (int i=0; i<array.length; i++)
+		//		String array[]= str.split(" ");
+		List<String> str1 = new ArrayList<String>( Arrays.asList(str.split(" ")));
+		List<Integer> num= new ArrayList<Integer>();
+		if(str1.contains("exit"))
 		{
-			try {
-				numberCount[count] = Integer.parseInt(array[i]);
-				System.out.println("the number- " + numberCount[count]);
-				count++;
-			}
-			catch(Exception ex)
+			System.out.println("Thank you for using Satapata Calculator");
+			System.exit(0);
+		}
+		else if(str1.contains("history"))
+		{
+			getHist(hist);
+		}
+		else 
+		{
+			Iterator<String> it= str1.iterator();
 			{
-				if (findStringValue(array[i]) != null)
+				while (it.hasNext())
 				{
-					findString = findStringValue(array[i]);
+					String str11 = it.next();
+					boolean temp= isInteger(str11);
+					if(temp)
+					{
+						int temp1 = Integer.parseInt(str11);
+						num.add(temp1);
+					}
 				}
-
 			}
+
+
+			int a=performCalculation(str1, num);
+			System.out.println("the ans is " +a);
+			String output = "the ans is "+a;
+			hist.add(str + " \n" + output);
+
+
 
 		}
-		try {
-			if (findString.equals("add"))
-			{
-				System.out.println(numberCount[0] + numberCount[1]);
-			}
-			else if (findString.equals("sub"))
-			{
-				System.out.println(numberCount[0] - numberCount[1]);
-			}
-			else if (findString.equals("mult"))
-			{
-				System.out.println(numberCount[0] * numberCount[1]);
-			}
-			else if (findString.equals("divide"))
-			{
-				System.out.println(numberCount[0] / numberCount[1]);
-			}
-		}
-		catch(Exception ex) {
-			System.out.println("Invalid Input, try again -");
-		}
 
-
+		loop(hist);
 	}
 
-	private static String findStringValue(String string) {
-
-		if(string.equals("add")|| string.equals("addition")||string.equals("sum") ||string.equals("+")|| string.equals("plus"))
+	private static void getHist(List<String> hist)
+	{
+		Iterator<String> c = hist.iterator();
+		while(c.hasNext())
 		{
-			return "add";
+			System.out.println(c.next());
 		}
-
-		else if(string.equals("sub")|| string.equals("subtract")|| string.equals("minus")|| string.equals("difference")|| string.equals("-"))
+	}
+	private static int performCalculation(List<String> str1, List<Integer> num )
+	{
+		if(str1.contains("add")||str1.contains("plus")||str1.contains("addition")||str1.contains("+") )
 		{
-			return "sub";
+			return num.get(0)+num.get(1);
 		}
-		else if(string.equals("multiply")|| string.equals("product")|| string.equals("times")|| string.equals("*"))
+		else if(str1.contains("subtract")||str1.contains("-")||str1.contains("difference")||str1.contains("sub"))
 		{
-			return "mult";
+			return num.get(0)- num.get(1);
 		}
-		else if(string.equals("divide")|| string.equals("by")|| string.equals("/")|| string.equals("quotient"))
+		else if(str1.contains("multiply")||str1.contains("*")||str1.contains("product"))
 		{
-			return "div";
+			return num.get(0)*num.get(1);
 		}
-
-		return null;
+		else if(str1.contains("divide")||str1.contains("quotient of")||str1.contains("division of")||str1.contains("/"))
+		{
+			return num.get(0)+num.get(1);
+		}
+		return 0;
 	}
 }
+
+
+
